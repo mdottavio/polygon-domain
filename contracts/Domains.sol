af70fd846ba7d9b49e2719702fc2d64fcba7246c
Mauricio Dottavio <dottavio@gmail.com>
2022-02-27 11:32:04 -0300

    chore: adding contract

---
+++ contracts/Domains.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("Domain contract,on");
+    }
+}
